0e39b9a4a5d8dd852b59f8cfbb133a3f68d40a97
Laurean021 <haritlau@gmail.com>
2025-06-17 23:21:36 -0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -6,11 +6,9 @@
                 <h4>Sobre Aventura Global</h4>
                 <p>Creamos experiencias de viaje inolvidables, cuidando cada detalle para que solo te preocupes por disfrutar.</p>
             </div>
-            <div class="footer-section">
-                <h4>Navegación</h4>
+            <div class="footer-section" id="footer">
+                <h4>Nuestras politicas</h4>
                 <ul>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Destinos</a></li>
                     <li><a href="#">Términos y Condiciones</a></li>
                     <li><a href="#">Política de Privacidad</a></li>
                 </ul>
@@ -18,9 +16,9 @@
             <div class="footer-section">
                 <h4>Síguenos</h4>
                 <div class="social-icons">
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.x.com"><i class="fab fa-x-twitter"></i></a>
                 </div>
             </div>
         </div>
